a4d38b7c135ef3c11e8535cdf62f0c642600255b
Thejaswininagaraju <92055809+Thejaswininagaraju@users.noreply.github.com>
2021-10-20 15:48:51 +0200

    Added sample springboot application

---
+++ src/main/java/com/javademo/boot_elk/HelloWorldSpringbootApplication.java
@@ -0,0 +1,10 @@
+package com.javademo.boot_elk;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringbootApplication.class, args);
+	}
+}
